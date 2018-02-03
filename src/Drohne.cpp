f19436d1627afc36857f1ef06292727b2b35fa14
Micha Eißler <arkanseidos@gmail.com>
2018-02-03 01:41:09 +0100

    Drohne.cpp formatiert/gesaeubert

--- src/Drohne.cpp
+++ src/Drohne.cpp
@@ -8,59 +8,6 @@
 #include <math.h>
 #include <helper_3dmath.h>
 #include <MPU6050_6Axis_MotionApps20.h>
-//#include <MPU6050_9Axis_MotionApps41.h>
-#include <math.h>
-#include <I2Cdev.h>
-
-// I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class using DMP (MotionApps v2.0)
-// 6/21/2012 by Jeff Rowberg <jeff@rowberg.net>
-// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
-//
-// Changelog:
-//      2013-05-08 - added seamless Fastwire support
-//                 - added note about gyro calibration
-//      2012-06-21 - added note about Arduino 1.0.1 + Leonardo compatibility error
-//      2012-06-20 - improved FIFO overflow handling and simplified read process
-//      2012-06-19 - completely rearranged DMP initialization code and simplification
-//      2012-06-13 - pull gyro and accel data from FIFO packet instead of reading directly
-//      2012-06-09 - fix broken FIFO read sequence and change interrupt detection to RISING
-//      2012-06-05 - add gravity-compensated initial reference frame acceleration output
-//                 - add 3D math helper file to DMP6 example sketch
-//                 - add Euler output and Yaw/Pitch/Roll output formats
-//      2012-06-04 - remove accel offset clearing for better results (thanks Sungon Lee)
-//      2012-06-01 - fixed gyro sensitivity to be 2000 deg/sec instead of 250
-//      2012-05-30 - basic DMP initialization working
-
-/* ============================================
-I2Cdev device library code is placed under the MIT license
-Copyright (c) 2012 Jeff Rowberg
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-===============================================
-*/
-
-// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
-// for both classes must be in the include path of your project
-#include "I2Cdev.h"
-
-#include "MPU6050_6Axis_MotionApps20.h"
-//#include "MPU6050.h" // not necessary if using MotionApps include file
 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
@@ -68,36 +15,8 @@ THE SOFTWARE.
 #include "Wire.h"
 #endif
 
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
-// AD0 high = 0x69
 MPU6050 mpu;
-//MPU6050 mpu(0x69); // <-- use for AD0 high
 
-/* =========================================================================
-NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
-depends on the MPU-6050's INT pin being connected to the Arduino's
-external interrupt #0 pin. On the Arduino Uno and Mega 2560, this is
-digital I/O pin 2.
-* ========================================================================= */
-
-/* =========================================================================
-NOTE: Arduino v1.0.1 with the Leonardo board generates a compile error
-when using Serial.write(buf, len). The Teapot output uses this method.
-The solution requires a modification to the Arduino USBAPI.h file, which
-is fortunately simple, but annoying. This will be fixed in the next IDE
-release. For more info, see these links:
-
-http://arduino.cc/forum/index.php/topic,109987.0.html
-http://code.google.com/p/arduino/issues/detail?id=958
-* ========================================================================= */
-
-// uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
-// pitch/roll angles (in degrees) calculated from the quaternions coming
-// from the FIFO. Note this also requires gravity vector calculations.
-// Also note that yaw/pitch/roll angles suffer from gimbal lock (for
-// more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
 //#define OUTPUT_READABLE_YAWPITCHROLL
 
 // MPU control/status vars
@@ -115,11 +34,6 @@ VectorFloat gravity; // [x, y, z]            gravity vector
 	float ypr[3];		 // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 #endif
 
-
-// packet structure for InvenSense teapot demo
-uint8_t teapotPacket[14] = {'$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n'};
-
-float duration, distance;
 
 Servo servo1;
 Servo servo2;
@@ -151,16 +65,12 @@ float rawAdjustRoll;
 
 float servoValue[3];
 
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
 void getMpuValues(float *data);
 void getTargetOrientation(float targetOrientation[]);
 float f(float x);
 void limitServoValues(float servoValue[]);
 float getAmplitudePercentage(float degreeDifference);
 int sgn(float val);
-void setupGyroAccel();
 
 volatile bool mpuInterrupt = false; // indicates whether MPU interrupt pin has gone high
 void dmpDataReady()
@@ -168,42 +78,23 @@ void dmpDataReady()
 	mpuInterrupt = true;
 }
 
-// ================================================================
-// ===                      INITIAL SETUP                       ===
-// ================================================================
-
 void setup()
 {
-	// initialize serial communication
-	// (115200 chosen because it is required for Teapot Demo output, but it's
-	// really up to you depending on your project)
 	Serial.begin(115200);
-	while (!Serial)
-		; // wait for Leonardo enumeration, others continue immediately
-
-	// NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
-	// Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
-	// the baud timing being too misaligned with processor ticks. You must use
-	// 38400 or slower in these cases, or use some kind of external separate
-	// crystal solution for the UART timer.
+	while (!Serial); // wait for Leonardo enumeration, otherwise continue immediately
 
 	servo1.attach(SERVO_1_PIN);
 	servo2.attach(SERVO_2_PIN);
 	servo3.attach(SERVO_3_PIN);
 	servo4.attach(SERVO_4_PIN);
 
-	setupGyroAccel();
-}
-
-void setupGyroAccel()
-{
 	// join I2C bus (I2Cdev library doesn't do this automatically)
-	#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-		Wire.begin();
-		TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
-	#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-		Fastwire::setup(400, true);
-	#endif
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+	Wire.begin();
+	TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+	Fastwire::setup(400, true);
+#endif
 
 	// initialize device
 	Serial.println(F("Initializing I2C devices..."));
@@ -260,13 +151,9 @@ void setupGyroAccel()
 	}
 }
 
-// ================================================================
-// ===                    MAIN PROGRAM LOOP                     ===
-// ================================================================
-
 void loop()
 {
-	servo4.write(180); //30 für Motor = aus
+	servo4.write(30); //30 für Motor = aus
 
 	getMpuValues(mpuData);
 	float targetOrientation[3];
@@ -352,14 +239,9 @@ void getMpuValues(float *data)
 	{
 		// other program behavior stuff here
 		// .
-		// .
-		// .
 		// if you are really paranoid you can frequently test in between other
 		// stuff to see if mpuInterrupt is true, and if so, "break;" from the
 		// while() loop to immediately process the MPU data
-		// .
-		// .
-		// .
 	}
 
 	// reset interrupt flag and get INT_STATUS byte
